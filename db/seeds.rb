121e9e33900cb5b22d26f5cc540b59ef7fcba054
vagrant <vagrant@kellogg-dev-box>
2014-04-18 16:08:44 +0000

    homework time

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,15 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
-#
+Book:
+- title: string
+- author: string
+
+Book.create(:title => "A Brief History of Time", :author => "Stephen Hawking")
+Book.create(:title => "Alice in Wonderland", :author => "Lewis Carroll")
+Book.create(:title => "The Lord Of The Rings", :author => "J.R.R. Tolkien")
+Book.create(:title => "The Hobbit", :author => "J.R.R. Tolkien")
+
+
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
