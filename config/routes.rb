d08dd0b509f2a4ba17a00aa2d6b50b1449319d85
vagrant <vagrant@kellogg-dev-box>
2014-05-11 23:38:28 +0000

    First commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Homework::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-get '/dice/roll' => 'dice/roll'
+get '/dice/roll' => 'dice#roll'
 
-get '/books' => 'books/list'
-get '/books/:book_id' => 'books/show'
+get '/books' => 'books#list'
+get '/books/:book_id' => 'books#show'
+
+get "/" => "books#homepage"
+
+
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
